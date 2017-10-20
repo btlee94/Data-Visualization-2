e4f1a1fc1a99a8b0d457bf74321f914873a15541
btlee94 <brandonl94@live.ca>
2017-10-19 21:31:28 -0600

    flower graphic

--- flower.js
+++ flower.js
@@ -1,15 +1,16 @@
 ﻿
-var data = blank;
-var petals = 15;
-var halfRadius = 75;
+var data = date;
 
 //width and height of svg element
 var width = window.innerWidth;
 var height = window.innerHeight;
+var petals = 15;
+var halfRadius = Math.min(width, height) / 5;
 var colour = d3.scaleOrdinal()
     .range(["#FF9E80", "#FF8A80", "#FF80AB", "#EA80FC", "#B388FF", "#8C9EFF", "#82B1FF", "#80D8FF", "#84FFFF", "#A7FFEB", "#B9F6CA", "#FFFF8D", "#FFE57F", "#FFD180", "#FFCCBC"]);
 
-var svg = d3.select("#pieChart")
+
+var svg = d3.select("#flower")
     .append("div")
     .classed("svg-container", true)
     .append("svg")
@@ -30,14 +31,14 @@ var path = svg.selectAll("path")
     .attr("transform", function (d) { return r((d.startAngle + d.endAngle) / 2); })
     .attr("d", petalPath)
     .attr("fill", function (d, i) { return colour(d.data.action); })
-    .each(function (d) { this._current = d; });
+    .style("stroke", "#616161");
 
 
 function petalPath(d) {
     var angle = (d.endAngle - d.startAngle) / 2,
         s = polarToCartesian(-angle, halfRadius),
         e = polarToCartesian(angle, halfRadius),
-        r = 90,
+        r = d.data.value * 13,
         m = { x: halfRadius + r, y: 0 },
         c1 = { x: halfRadius + r / 2, y: s.y },
         c2 = { x: halfRadius + r / 2, y: e.y };
@@ -56,119 +57,17 @@ function polarToCartesian(angle, radius) {
     };
 }
 
-var legendRectSize = radius / 9;
-var legendSpacing = radius / 45;
-
-var legend = svg.selectAll(".legend")
-    .data(colour.domain())
-    .enter()
-    .append("g")
-    .attr("class", "legend")
-    .attr("transform", function (d, i) {
-        var height = legendRectSize + legendSpacing;
-        var offset = radius;
-        var horizontal = radius + (legendRectSize * 3);
-        var vertical = i * height - offset;
-        return "translate(" + horizontal + "," + vertical + ")";
-    });
-
-legend.append("rect")
-    .attr("width", legendRectSize)
-    .attr("height", legendRectSize)
-    .style("fill", colour)
-    .style("stroke", colour);
-
-legend.append("text")
-    .attr("x", legendRectSize + legendSpacing)
-    .attr("y", legendRectSize / 1.5)
-    .style("fill", "#c9d0d4")
-    .style("font-family", "'Verdana', sans-serif")
-    .style("font-size", "15")
-    .text(function (d) { return d; });
-
-var selection = svg.selectAll(".selection")
-    .data(situations)
-    .enter()
-    .append("g")
-    .attr("class", "selection")
-    .attr("transform", function (d, i) {
-        var height = legendRectSize + legendSpacing;
-        var offset = radius;
-        var horizontal = (radius + (legendRectSize * 7.5)) * -1;
-        var vertical = i * height - offset;
-        return "translate(" + horizontal + "," + vertical + ")";
-    });
-
-selection.append("circle")
-    .attr("cx", (legendRectSize / 2))
-    .attr("cy", (legendRectSize / 2))
-    .attr("r", (legendRectSize / 2))
-    .attr("class", "disabled")
-    .attr("name", function (d) { return d; })
-    .on("click", function (action) {
-        var circle = d3.select(this);
-        var items = document.querySelectorAll(".selection");
-        for (var i = 0; i < items.length; i++) {
-            if (items[i].firstChild.getAttribute("class") == "enabled")
-                items[i].firstChild.setAttribute("class", "disabled");
-         }
-        
-        if (circle.attr("class") == "disabled") {
-            circle.attr("class", "enabled");
-        }
-        if (circle.attr("name") == "Class")
-            data = school;
-        else if (circle.attr("name") == "Date")
-            data = date;
-        else if (circle.attr("name") == "Bus")
-            data = bus;
-        else if (circle.attr("name") == "Family Dinner")
-            data = familyDinner;
-        else if (circle.attr("name") == "Park")
-            data = park;
-        else if (circle.attr("name") == "Church")
-            data = church;
-        else if (circle.attr("name") == "Job Interview")
-            data = jobInterview;
-        else if (circle.attr("name") == "Sidewalk")
-            data = sidewalk;
-        else if (circle.attr("name") == "Movies")
-            data = movies;
-        else if (circle.attr("name") == "Bar")
-            data = bar;
-        else if (circle.attr("name") == "Elevator")
-            data = elevator;
-        else if (circle.attr("name") == "Restroom")
-            data = restroom;
-        else if (circle.attr("name") == "Own Room")
-            data = ownRoom;
-        else if (circle.attr("name") == "Dorm Lounge")
-            data = dorm;
-        else if (circle.attr("name") == "Football Game")
-            data = footballGame;
-        else
-            data = blank;
-            
-
-        path = path.data(pie(data));
-
-        path.transition().duration(900)
-            .attr("transform", function (d) { return r((d.startAngle + d.endAngle) / 2); })
-            .attr("d", petalPath);
-            
-    });
 
 
-selection.append("text")
-    .attr("x", legendRectSize + legendSpacing)
-    .attr("y", legendRectSize / 1.5)
-    .style("fill", "#c9d0d4")
-    .style("font-family", "'Verdana', sans-serif")
-    .style("font-size", "15")
-    .text(function (d) { return d; });
+ /*
+path = path.data(pie(data));
 
-
-var tip = d3.select("#chart")
+path.transition().duration(900)
+    .attr("transform", function (d) { return r((d.startAngle + d.endAngle) / 2); })
+    .attr("d", petalPath);
+ */
+   
+var tip = d3.select("#flower")
     .data(data)
     .enter()
     .append("div")
